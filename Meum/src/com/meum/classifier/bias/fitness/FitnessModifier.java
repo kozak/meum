dc8d5f9b56057a3ef9c05f19927fe87ffa386622
Morswin <Morswin@34733b08-dd3e-11de-b357-fb893c5a43fa>
2010-01-24 19:27:41 +0000

    Ensemble based modifier. Adjusts the individuals fitness based on its contribution to the result given by entire population.

--- Meum/src/com/meum/classifier/bias/fitness/FitnessModifier.java
+++ Meum/src/com/meum/classifier/bias/fitness/FitnessModifier.java
@@ -1,11 +1,21 @@
 package com.meum.classifier.bias.fitness;
 
+import com.meum.classifier.Fitness;
 import com.meum.classifier.TreeNode;
 import com.meum.classifier.bias.BiasModifier;
 
 import java.util.List;
 
 public abstract class FitnessModifier extends BiasModifier {
+    private Fitness fitness;
+
+    public Fitness getFitness() {
+        return fitness;
+    }
+
+    public void setFitness(Fitness fitness) {
+        this.fitness = fitness;
+    }
 
     public abstract double adjustFitness(double fitness, TreeNode candidate, List<? extends TreeNode> population);
 }
--- Meum/src/com/meum/classifier/test/RealDataTests.java
+++ Meum/src/com/meum/classifier/test/RealDataTests.java
@@ -2,6 +2,7 @@ package com.meum.classifier.test;
 
 import com.meum.classifier.*;
 import com.meum.classifier.analysis.PriceBasedBuySellChooser;
+import com.meum.classifier.bias.fitness.EnsembleModifier;
 import com.meum.classifier.bias.fitness.HeightBasedModifier;
 import com.meum.classifier.bias.mutation.RangeBiasModifier;
 import com.meum.classifier.utils.CsvReader;
@@ -55,6 +56,41 @@ public class RealDataTests extends EvolutionTest {
         final TestConfig config = new TestConfig("Single market test",
                 2, 1000, 20, conditions,
                 fitness, 
+                operators,
+                observers,
+                new MersenneTwisterRNG(),
+                new RouletteWheelSelection());
+        evolve(factory, config, trainingData, null);
+    }
+
+    @Test
+    public void testEnsembleSingleMarket() throws IOException {
+        final double[] prices = reader.read("audeur");
+        final PriceBasedBuySellChooser chooser = new PriceBasedBuySellChooser(prices);
+        final Map<double[],Target> trainingData = chooser.getTrainingData(15, 0.005, 15);
+        DecisionTreeFactory factory = new DecisionTreeFactory(trainingData, 2);
+        List<EvolutionaryOperator<TreeNode>> operators = new ArrayList<EvolutionaryOperator<TreeNode>>(2);
+        final RangeBiasModifier mutationModifier = new RangeBiasModifier(
+                new RangeBiasModifier.Range(0, 50, 0),
+                new RangeBiasModifier.Range(50, 100, 1),
+                new RangeBiasModifier.Range(100, 150, 0),
+                new RangeBiasModifier.Range(150, 200, 2)
+        );
+        operators.add(new Mutation(new Probability(0.6), factory, mutationModifier));
+        operators.add(new Crossover());
+        operators.add(new Simplification());
+
+        final EnsembleModifier ensembleModifier = new EnsembleModifier();
+        final Fitness fitness = new Fitness(trainingData, ensembleModifier);
+
+        List<EvolutionObserver<TreeNode>> observers = new ArrayList<EvolutionObserver<TreeNode>>(1);
+        observers.add(mutationModifier);
+        observers.add(ensembleModifier);
+
+        final TerminationCondition[] conditions = {new GenerationCount(200), new TargetFitness(2, false)};
+        final TestConfig config = new TestConfig("Single market test",
+                2, 200, 20, conditions,
+                fitness,
                 operators,
                 observers,
                 new MersenneTwisterRNG(),
